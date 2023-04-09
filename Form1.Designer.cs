b954677d970b2307f2422dd4029d9146d969ed36
VertiBulki <108471555+VertiBulki@users.noreply.github.com>
2023-04-09 11:33:56 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace MemoryGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox2.Location = new System.Drawing.Point(183, 13);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.pictureBox3.Location = new System.Drawing.Point(13, 183);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.pictureBox4.Location = new System.Drawing.Point(183, 183);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(10);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(150, 150);
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(346, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 38);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(346, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(214, 109);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ожидание...";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(346, 183);
+            this.trackBar1.Maximum = 20;
+            this.trackBar1.Minimum = 3;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(211, 56);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Value = 3;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(531, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 29);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "3";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(351, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "количество плиток:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 405);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,421 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace MemoryGame
+{
+    public partial class Form1 : Form
+    {
+        Random rn = new Random();
+
+        int startCount = 1;
+
+        int currentCount = 0;
+
+        int max = 0;
+
+        int f = 0;
+
+        bool islose = false;
+
+        bool isStarted = false;
+        
+        bool isGo = false;
+
+        bool atstart = false;
+
+        bool flag1 = false;
+        bool flag2 = false;
+        bool flag3 = false;
+        bool flag4 = false;
+
+        bool isOpened1 = false;
+        bool isOpened2 = false;
+        bool isOpened3 = false;
+        bool isOpened4 = false;
+
+        MediaPlayer pl1 = new MediaPlayer();
+        MediaPlayer pl2 = new MediaPlayer();
+        MediaPlayer pl3 = new MediaPlayer();
+        MediaPlayer pl4 = new MediaPlayer();
+
+        PictureBox[] ranBoxes = new PictureBox[4];
+        
+        PictureBox[] arr = new PictureBox[3];
+        PictureBox[] currentarr = new PictureBox[3];
+        
+        public Form1()
+        {
+            InitializeComponent();
+            ranBoxes[0] = pictureBox1;
+            ranBoxes[1] = pictureBox2;
+            ranBoxes[2] = pictureBox3;
+            ranBoxes[3] = pictureBox4;
+        }
+
+         private void button1_Click_1(object sender, EventArgs e)
+        {
+            islose = false;
+            atstart = true;
+            this.KeyPreview = false;
+            trackBar1.Enabled = false;
+            label1.Text = "";
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+            pictureBox2.BackColor = System.Drawing.Color.FromArgb(128, 255, 128);
+            pictureBox3.BackColor = System.Drawing.Color.FromArgb(128, 128, 255);
+            pictureBox4.BackColor = System.Drawing.Color.FromArgb(255, 255, 128);
+            startCount = 1;
+            f = 0;
+            currentCount = 0;
+            isStarted = false;
+            Game_Meth();
+            
+        }
+        async public void Game_Meth()
+        {
+            trackBar1.Enabled = false;
+
+            isGo = true;
+            
+            button1.Enabled = false;
+
+            this.KeyPreview = false;
+
+            if (isStarted == false) //заполнение массива плиток
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    arr[i] = ranBoxes[rn.Next(4)];
+                }
+            }
+            isStarted = true;
+            await Task.Delay(500);
+            for (int i = 0; i < startCount; i++)
+            {
+                
+                await Task.Delay(300);
+                if (startCount-1 == arr.Length)
+                {
+                    label1.Text = $"у вас {startCount-1} очка. уровень пройден";
+                    button1.Enabled = true;
+                    trackBar1.Enabled = true;
+                    max = 0;
+                    return;
+                }
+                if (arr[i].BackColor == System.Drawing.Color.FromArgb(255, 128, 128))
+                {
+                    if (isOpened1 == false)
+                    { pl1.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\do1.wav")); isOpened1 = true; }
+                    pl1.Position = TimeSpan.Zero;
+                    pl1.Play();
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(192, 0, 0);
+                    await Task.Delay(500);
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                }
+
+                else if (arr[i].BackColor == System.Drawing.Color.FromArgb(128, 255, 128))
+                {
+                    if (isOpened2 == false)
+                    { pl2.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\re1.wav")); isOpened2 = true; }
+                    pl2.Position = TimeSpan.Zero;
+                    pl2.Play();
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(0, 192, 0);
+                    await Task.Delay(500);
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(128, 255, 128);
+                }
+
+                else if (arr[i].BackColor == System.Drawing.Color.FromArgb(128, 128, 255))
+                {
+                    if (isOpened3 == false)
+                    { pl3.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\mi1.wav")); isOpened3 = true; }
+                    pl3.Position = TimeSpan.Zero;
+                    pl3.Play();
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(0, 0, 192);
+                    await Task.Delay(500);
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(128, 128, 255);
+                }
+
+                else
+                {
+                    if (isOpened4 == false)
+                    { pl4.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\fa1.wav")); isOpened4 = true; }
+                    pl4.Position = TimeSpan.Zero;
+                    pl4.Play();
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(192, 192, 0);
+                    await Task.Delay(500);
+                    arr[i].BackColor = System.Drawing.Color.FromArgb(255, 255, 128);
+                }
+            } //демонстрация нажатия плиток в последовательности
+
+
+
+            this.KeyPreview = false;
+            await Task.Delay(500);
+            this.KeyPreview = true;
+            button1.Enabled = true;
+            startCount++;
+            isGo = false;
+            trackBar1.Enabled = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.KeyPreview = false;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            DownTile(e);
+            //Game_Meth();
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            UpTile(e);
+            
+        }
+        
+         public void DownTile(KeyEventArgs ev)
+        {
+            switch (ev.KeyCode)
+            {
+                case Keys.Q:
+                    if (flag1 == true)
+                        return;
+                    flag1 = true;
+                    pictureBox1.BackColor = System.Drawing.Color.FromArgb(192, 0, 0);
+                    if(isOpened1 == false)
+                    { pl1.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\do1.wav")); isOpened1 = true; }
+                    pl1.Position = TimeSpan.Zero;
+                    pl1.Play();
+                    currentarr[currentCount] = pictureBox1;
+                    
+                    
+                    //after_Switch();
+                    
+                    return;
+                case Keys.W:
+                    if (flag2 == true)
+                        return;
+                    flag2 = true;
+                    pictureBox2.BackColor = System.Drawing.Color.FromArgb(0,192,0);
+                    if (isOpened2 == false)
+                    { pl2.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\re1.wav")); isOpened2 = true; }
+                    pl2.Position = TimeSpan.Zero;
+                    pl2.Play();
+                    currentarr[currentCount] = pictureBox2;
+                    //after_Switch();
+                    return;
+                case Keys.E:
+                    if (flag3 == true)
+                        return;
+                    flag3 = true;
+                    pictureBox3.BackColor = System.Drawing.Color.FromArgb(0,0,192);
+                    if (isOpened3 == false)
+                    { pl3.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\mi1.wav")); isOpened3 = true; }
+                    pl3.Position = TimeSpan.Zero;
+                    pl3.Play();
+                    currentarr[currentCount] = pictureBox3;
+                    //after_Switch();
+                    return;
+                case Keys.R:
+                    if (flag4 == true)
+                        return;
+                    flag4 = true;
+                    pictureBox4.BackColor = System.Drawing.Color.FromArgb(192, 192, 0);
+                    if (isOpened4 == false)
+                    { pl4.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\fa1.wav")); isOpened4 = true; }
+                    pl4.Position = TimeSpan.Zero;
+                    pl4.Play();
+                    currentarr[currentCount] = pictureBox4;
+                    //after_Switch();
+                    return;
+
+
+            }
+            
+            
+            
+            //Game_Meth();
+        }
+
+        public void after_Switch()
+        {
+            if(max < currentCount)
+                max = currentCount;
+            if(f == currentCount)
+            {
+                for (int i = 0; i < currentCount + 1; i++) // проверка массивов на равные элементы, иначе игра окончена
+                {
+                    if (currentarr[i] != arr[i])
+                    {
+                        islose = true;
+                        label1.Text = $"вы проебали со счетом {max}";
+                        //Array.Clear(arr, 0, arr.Length);
+                        //Array.Clear(currentarr, 0, currentarr.Length);
+                        Reset();
+                        max = 0;
+                        currentCount = 0;
+                        startCount = 1;
+                        trackBar1.Enabled = true;
+                        f = 0;
+                        return;
+                    }
+
+
+                }
+                currentCount = 0;
+                if(islose == false)
+                    Game_Meth();
+                f++;
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < currentCount + 1; i++) // проверка массивов на равные элементы, иначе игра окончена
+                {
+                    if (currentarr[i] != arr[i])
+                    {
+                        islose = true;
+                        label1.Text = $"вы проебали со счетом {max}";
+                        //Array.Clear(arr, 0, arr.Length);
+                        //Array.Clear(currentarr, 0, arr.Length);
+                        Reset();
+                        max = 0;
+                        currentCount = 0;
+                        startCount = 1;
+                        trackBar1.Enabled = true;
+                        f = 0;
+                        return;
+                    }
+
+
+                }
+                currentCount++;
+            }
+            
+        }
+        public void Reset()
+        {
+            this.KeyPreview = false;
+            
+        }
+        public void UpTile(KeyEventArgs ev)
+        {
+            switch (ev.KeyCode)
+            {
+                case Keys.Q:
+                    flag1 = false;
+                    pictureBox1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                    after_Switch();
+                    return;
+                case Keys.W:
+                    flag2 = false;
+                    pictureBox2.BackColor = System.Drawing.Color.FromArgb(128,255,128);
+                    after_Switch();
+                    return;
+                case Keys.E:
+                    flag3 = false;
+                    pictureBox3.BackColor = System.Drawing.Color.FromArgb(128,128,255);
+                    after_Switch();
+                    return;
+                case Keys.R:
+                    flag4 = false;
+                    pictureBox4.BackColor = System.Drawing.Color.FromArgb(255,255,128);
+                    after_Switch();
+                    return;
+            }
+        }
+
+        async private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            if(isGo == false)
+            {
+                pictureBox1.BackColor = System.Drawing.Color.FromArgb(192, 0, 0);
+                if (isOpened1 == false)
+                { pl1.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\do1.wav")); isOpened1 = true; }
+                pl1.Position = TimeSpan.Zero;
+                pl1.Play();
+                currentarr[currentCount] = pictureBox1;
+                await Task.Delay(150);
+                pictureBox1.BackColor = System.Drawing.Color.FromArgb(255, 128, 128);
+                if (atstart)
+                    after_Switch();
+            }
+        }
+
+        async private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+            if (isGo == false)
+            {
+                pictureBox2.BackColor = System.Drawing.Color.FromArgb(0, 192, 0);
+                if (isOpened2 == false)
+                { pl2.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\re1.wav")); isOpened2 = true; }
+                pl2.Position = TimeSpan.Zero;
+                pl2.Play();
+                currentarr[currentCount] = pictureBox2;
+                await Task.Delay(150);
+                pictureBox2.BackColor = System.Drawing.Color.FromArgb(128, 255, 128);
+                if(atstart)
+                    after_Switch();
+            }
+        }
+
+        async private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            if(isGo == false)
+            {
+                pictureBox3.BackColor = System.Drawing.Color.FromArgb(0, 0, 192);
+                if (isOpened3 == false)
+                { pl3.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\mi1.wav")); isOpened3 = true; }
+                pl3.Position = TimeSpan.Zero;
+                pl3.Play();
+                currentarr[currentCount] = pictureBox3;
+                await Task.Delay(150);
+                pictureBox3.BackColor = System.Drawing.Color.FromArgb(128, 128, 255);
+                if (atstart)
+                    after_Switch();
+            }
+            
+        }
+
+        async private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            if (isGo == false)
+            {
+                pictureBox4.BackColor = System.Drawing.Color.FromArgb(192, 192, 0);
+                if (isOpened4 == false)
+                { pl4.Open(new Uri(@"D:\работы в csharp\MemoryGame\MemoryGame\Resources\fa1.wav")); isOpened4 = true; }
+                pl4.Position = TimeSpan.Zero;
+                pl4.Play();
+                currentarr[currentCount] = pictureBox4;
+                await Task.Delay(150);
+                pictureBox4.BackColor = System.Drawing.Color.FromArgb(255, 255, 128);
+                if (atstart)
+                    after_Switch();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label2.Text = trackBar1.Value.ToString();
+            arr = new PictureBox[trackBar1.Value];
+            currentarr = new PictureBox[trackBar1.Value];
+            max = 0;
+            currentCount = 0;
+            startCount = 1;
+            label1.Text = "ожидание...";
+            atstart = false;
+        }
+    }
+}
